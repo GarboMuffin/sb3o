1f0d7fcb6244464b42e04f95fa2ba8af59cadba8
Thomas Weber <weber_t@mailbox.org>
2021-03-13 22:25:22 -0600

    Disable svgo plugin removeViewBox

--- src/optimizations/optimize-svg.js
+++ src/optimizations/optimize-svg.js
@@ -4,6 +4,44 @@ const AssetOptimization = require('./asset-optimizer');
 const decoder = new TextDecoder();
 const encoder = new TextEncoder();
 
+const plugins = [
+  // Enabled by default
+  'removeDoctype',
+  'removeXMLProcInst',
+  'removeComments',
+  'removeMetadata',
+  'removeEditorsNSData',
+  'cleanupAttrs',
+  'inlineStyles',
+  'minifyStyles',
+  'cleanupIDs',
+  'removeUselessDefs',
+  'cleanupNumericValues',
+  'convertColors',
+  'removeUnknownsAndDefaults',
+  'removeNonInheritableGroupAttrs',
+  'removeUselessStrokeAndFill',
+  // do not remove viewbox
+  // 'removeViewBox',
+  'cleanupEnableBackground',
+  'removeHiddenElems',
+  'removeEmptyText',
+  'convertShapeToPath',
+  'convertEllipseToCircle',
+  'moveElemsAttrsToGroup',
+  'moveGroupAttrsToElems',
+  'collapseGroups',
+  'convertPathData',
+  'convertTransform',
+  'removeEmptyAttrs',
+  'removeEmptyContainers',
+  'mergePaths',
+  'removeUnusedNS',
+  'sortDefsChildren',
+  'removeTitle',
+  'removeDesc'
+];
+
 class SVGOptimizer extends AssetOptimization {
   shouldOptimize(asset) {
     return asset.dataFormat === 'svg';
@@ -11,7 +49,9 @@ class SVGOptimizer extends AssetOptimization {
 
   optimizeAsset(data) {
     const decoded = decoder.decode(data);
-    const optimized = svgo.optimize(decoded);
+    const optimized = svgo.optimize(decoded, {
+      plugins
+    });
     return encoder.encode(optimized.data);
   }
 }
