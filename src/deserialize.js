bcd1037491407342c1b2e0225a55404818724196
Thomas Weber <weber_t@mailbox.org>
2021-03-17 11:01:16 -0500

    Handle project.json being in subdirectly

--- src/deserialize.js
+++ src/deserialize.js
@@ -3,7 +3,12 @@ const {parse} = require('./json');
 
 const deserialize = async (binaryProject) => {
   const zip = await JSZip.loadAsync(binaryProject);
-  const projectDataText = await zip.file('project.json').async('text');
+  const projectJSONFile = zip.file(/project\.json/)[0];
+  if (!projectJSONFile) {
+    throw new Error('project.json does not exist');
+  }
+  const pathPrefix = projectJSONFile.name.substr(0, projectJSONFile.name.indexOf('project.json'));
+  const projectDataText = await projectJSONFile.async('text');
   const projectData = parse(projectDataText);
   const assets = new Map();
   for (const file of Object.keys(zip.files)) {
@@ -13,7 +18,7 @@ const deserialize = async (binaryProject) => {
       /^[a-z0-9]{32}\.wav$/.test(file) ||
       /^[a-z0-9]{32}\.mp3$/.test(file)
     ) {
-      assets.set(file, await zip.file(file).async('arraybuffer'));
+      assets.set(file, await zip.file(pathPrefix + file).async('arraybuffer'));
     }
   }
   return {
